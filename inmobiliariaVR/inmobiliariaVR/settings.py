009c741883a8915c6b7ebc30adf51dc8d66ca9fe
Jose Gomez Baena <jose.gob@gmail.com>
2018-04-10 20:03:44 +0200

    Arreglo ruta JS

--- inmobiliariaVR/inmobiliariaVR/settings.py
+++ inmobiliariaVR/inmobiliariaVR/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-JS_URL = 'inmobiliariaVR/static/js'
-JS_ROOT = os.path.join(BASE_DIR, 'inmobiliariaVR\static\js')
+JS_URL = '/inmobiliariaVR/static/js'
+JS_ROOT = os.path.join(BASE_DIR, 'inmobiliariaVR/static/js')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
